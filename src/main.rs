e0b4f39872cc9665b47e6525215742f3793681ac
David Tolnay <dtolnay@gmail.com>
2017-04-24 20:42:34 -0700

    Prepare for lifetimes on structs

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ extern crate serde_derive;
 extern crate dtoa;
 extern crate time;
 
-#[cfg(feature = "lib-rustc-serialize")]
-extern crate rustc_serialize;
 #[cfg(feature = "lib-serde")]
 extern crate serde;
+#[cfg(feature = "lib-serde")]
+extern crate serde_json;
 
-#[macro_use] pub mod enums;
+#[cfg(feature = "lib-json-rust")]
+extern crate json;
+
+#[cfg(feature = "lib-rustc-serialize")]
+extern crate rustc_serialize;
+
+#[macro_use]
+pub mod enums;
 
 pub mod adapter;
 pub mod color;
@@ -32,77 +39,6 @@ pub use std::io::{self, Read, Write};
 use std::env;
 use std::fs::File;
 
-#[derive(Copy, Clone)]
-pub enum TestFile {
-    #[cfg(feature = "file-canada")]
-    Canada,
-    #[cfg(feature = "file-citm-catalog")]
-    CitmCatalog,
-    #[cfg(feature = "file-twitter")]
-    Twitter,
-}
-
-impl TestFile {
-    #[allow(unused_mut)]
-    pub fn files() -> Vec<TestFile> {
-        let mut variants = Vec::new();
-        #[cfg(feature = "file-canada")]
-        variants.push(TestFile::Canada);
-        #[cfg(feature = "file-citm-catalog")]
-        variants.push(TestFile::CitmCatalog);
-        #[cfg(feature = "file-twitter")]
-        variants.push(TestFile::Twitter);
-        variants
-    }
-
-    pub fn path(self) -> &'static str {
-        match self {
-            #[cfg(feature = "file-canada")]
-            TestFile::Canada => "data/canada.json",
-            #[cfg(feature = "file-citm-catalog")]
-            TestFile::CitmCatalog => "data/citm_catalog.json",
-            #[cfg(feature = "file-twitter")]
-            TestFile::Twitter => "data/twitter.json",
-        }
-    }
-}
-
-pub enum Contents {
-    #[cfg(feature = "file-canada")]
-    Canada(canada::Canada),
-    #[cfg(feature = "file-citm-catalog")]
-    CitmCatalog(citm_catalog::CitmCatalog),
-    #[cfg(feature = "file-twitter")]
-    Twitter(twitter::Twitter),
-}
-
-trait Library {
-    type DOM;
-
-    fn name() -> &'static str;
-
-    fn parse_dom<R>(read: R) -> Result<Self::DOM, ()>
-        where R: io::Read;
-    fn stringify_dom<W>(write: &mut W, dom: &Self::DOM)
-        where W: io::Write;
-
-    fn can_codegen() -> bool { false }
-    fn parse_struct<R>(R, TestFile) -> Contents
-        where R: io::Read { unimplemented!() }
-    fn stringify_struct<W>(&mut W, &Contents)
-        where W: io::Write { unimplemented!() }
-}
-
-fn main() {
-    print!("{:>35}{:>22}", "DOM", "STRUCT");
-    #[cfg(feature = "lib-serde")]
-    bench::<serde_json::Library>();
-    #[cfg(feature = "lib-json-rust")]
-    bench::<json_rust::Library>();
-    #[cfg(feature = "lib-rustc-serialize")]
-    bench::<rustc::Library>();
-}
-
 fn num_trials() -> usize {
     extern crate getopts;
     let mut opts = getopts::Options::new();
@@ -113,76 +49,194 @@ fn num_trials() -> usize {
     matches.opt_str("n").map(|s| s.parse().unwrap()).unwrap_or(256)
 }
 
-fn bench<L: Library>() {
-    let name = format!(" {} ", L::name());
-    println!("\n{:=^26} parse|stringify === parse|stringify ===", name);
-    for file in TestFile::files() {
-        print!("{:22}", file.path());
-        io::stdout().flush().unwrap();
+macro_rules! bench {
+    {
+        name: $name:expr,
+        $($args:tt)*
+    } => {
+        let name = format!(" {} ", $name);
+        println!("\n{:=^26} parse|stringify === parse|stringify ===", name);
 
-        let mut contents = Vec::new();
-        File::open(file.path()).unwrap().read_to_end(&mut contents).unwrap();
-        let dom = L::parse_dom(contents.as_slice()).unwrap();
-
-        if cfg!(feature = "parse-dom") {
-            let dur = bench_parse_dom::<L>(contents.as_slice());
-            print!("{:6}.{}ms", millis(dur), tenths(dur));
-            io::stdout().flush().unwrap();
-        } else {
-            print!("          ");
+        #[cfg(feature = "file-canada")]
+        bench_file! {
+            path: "data/canada.json",
+            structure: canada::Canada,
+            $($args)*
         }
 
-        if cfg!(feature = "stringify-dom") {
-            let dur = bench_stringify_dom::<L>(&dom, contents.len());
-            print!("{:6}.{}ms", millis(dur), tenths(dur));
-            io::stdout().flush().unwrap();
-        } else {
-            print!("          ");
+        #[cfg(feature = "file-citm-catalog")]
+        bench_file! {
+            path: "data/citm_catalog.json",
+            structure: citm_catalog::CitmCatalog,
+            $($args)*
         }
 
-        if L::can_codegen() {
-            if cfg!(feature = "parse-struct") {
-                let dur = bench_parse_struct::<L>(contents.as_slice(), file);
-                print!("{:6}.{}ms", millis(dur), tenths(dur));
-                io::stdout().flush().unwrap();
-            } else {
-                print!("          ");
-            }
-
-            if cfg!(feature = "stringify-struct") {
-                let parsed = L::parse_struct(contents.as_slice(), file);
-                let dur = bench_stringify_struct::<L>(parsed, contents.len());
-                print!("{:6}.{}ms", millis(dur), tenths(dur));
-                io::stdout().flush().unwrap();
-            }
+        #[cfg(feature = "file-twitter")]
+        bench_file! {
+            path: "data/twitter.json",
+            structure: twitter::Twitter,
+            $($args)*
         }
-
-        println!("");
     }
 }
 
-fn bench_parse_dom<L: Library>(contents: &[u8]) -> time::Duration {
-    timer::bench(num_trials(), || {
-        L::parse_dom(contents).ok();
-    })
+macro_rules! bench_file {
+    {
+        path: $path:expr,
+        structure: $structure:ty,
+        dom: $dom:ty,
+        parse_dom: $parse_dom:expr,
+        stringify_dom: $stringify_dom:expr,
+        $(
+            parse_struct: $parse_struct:expr,
+            stringify_struct: $stringify_struct:expr,
+        )*
+    } => {
+        print!("{:22}", $path);
+        io::stdout().flush().unwrap();
+
+        let contents = {
+            let mut vec = Vec::new();
+            File::open($path).unwrap().read_to_end(&mut vec).unwrap();
+            vec
+        };
+
+        let len = contents.len();
+        let dom: $dom = $parse_dom(&contents).unwrap();
+
+        #[cfg(feature = "parse-dom")]
+        {
+            let dur = timer::bench(num_trials(), || {
+                let parsed: $dom = $parse_dom(&contents).unwrap();
+                parsed
+            });
+            print!("{:6}.{}ms", millis(dur), tenths(dur));
+            io::stdout().flush().unwrap();
+        }
+        #[cfg(not(feature = "parse-dom"))]
+        print!("          ");
+
+        #[cfg(feature = "stringify-dom")]
+        {
+            let dur = timer::bench_with_buf(num_trials(), len, |out| {
+                $stringify_dom(out, &dom).unwrap()
+            });
+            print!("{:6}.{}ms", millis(dur), tenths(dur));
+            io::stdout().flush().unwrap();
+        }
+        #[cfg(not(feature = "stringify-dom"))]
+        print!("          ");
+
+        $(
+            #[cfg(feature = "parse-struct")]
+            {
+                let dur = timer::bench(num_trials(), || {
+                    let parsed: $structure = $parse_struct(&contents).unwrap();
+                    parsed
+                });
+                print!("{:6}.{}ms", millis(dur), tenths(dur));
+                io::stdout().flush().unwrap();
+            }
+            #[cfg(not(feature = "parse-struct"))]
+            print!("          ");
+
+            #[cfg(feature = "stringify-struct")]
+            {
+                let parsed: $structure = $parse_struct(&contents).unwrap();
+                let dur = timer::bench_with_buf(num_trials(), len, |out| {
+                    $stringify_struct(out, &parsed).unwrap()
+                });
+                print!("{:6}.{}ms", millis(dur), tenths(dur));
+                io::stdout().flush().unwrap();
+            }
+        )*
+
+        println!();
+    }
 }
 
-fn bench_stringify_dom<L: Library>(dom: &L::DOM, len: usize) -> time::Duration {
-    timer::bench_with_buf(num_trials(), len, |out| {
-        L::stringify_dom(out, &dom)
-    })
+fn main() {
+    print!("{:>35}{:>22}", "DOM", "STRUCT");
+
+    #[cfg(feature = "lib-serde")]
+    bench! {
+        name: "serde_json",
+        dom: serde_json::Value,
+        parse_dom: serde_json_parse_dom,
+        stringify_dom: serde_json::to_writer,
+        parse_struct: serde_json_parse_struct,
+        stringify_struct: serde_json::to_writer,
+    }
+
+    #[cfg(feature = "lib-json-rust")]
+    bench! {
+        name: "json-rust",
+        dom: json::JsonValue,
+        parse_dom: json_rust_parse_dom,
+        stringify_dom: json_rust_stringify_dom,
+    }
+
+    #[cfg(feature = "lib-rustc-serialize")]
+    bench! {
+        name: "rustc_serialize",
+        dom: rustc_serialize::json::Json,
+        parse_dom: rustc_serialize_parse_dom,
+        stringify_dom: rustc_serialize_stringify,
+        parse_struct: rustc_serialize_parse_struct,
+        stringify_struct: rustc_serialize_stringify,
+    }
 }
 
-fn bench_parse_struct<L: Library>(contents: &[u8], which: TestFile) -> time::Duration {
-    timer::bench(num_trials(), || {
-        L::parse_struct(contents, which)
-    })
+#[cfg(all(feature = "lib-serde", feature = "parse-dom"))]
+fn serde_json_parse_dom(bytes: &[u8]) -> serde_json::Result<serde_json::Value> {
+    use std::str;
+    let s = str::from_utf8(bytes).unwrap();
+    serde_json::from_str(s)
 }
 
-fn bench_stringify_struct<L: Library>(contents: Contents, len: usize) -> time::Duration {
-    timer::bench_with_buf(num_trials(), len, |out| {
-        L::stringify_struct(out, &contents)
-    })
+#[cfg(all(feature = "lib-serde", feature = "parse-struct"))]
+fn serde_json_parse_struct<T>(bytes: &[u8]) -> serde_json::Result<T>
+    where T: serde::Deserialize
+{
+    use std::str;
+    let s = str::from_utf8(bytes).unwrap();
+    serde_json::from_str(s)
+}
+
+#[cfg(all(feature = "lib-json-rust", feature = "parse-dom"))]
+fn json_rust_parse_dom(bytes: &[u8]) -> json::Result<json::JsonValue> {
+    use std::str;
+    let s = str::from_utf8(bytes).unwrap();
+    json::parse(&s)
+}
+
+#[cfg(all(feature = "lib-json-rust", feature = "stringify-dom"))]
+fn json_rust_stringify_dom<W: io::Write>(write: &mut W, dom: &json::JsonValue) -> io::Result<()> {
+    dom.write(write)
+}
+
+#[cfg(all(feature = "lib-rustc-serialize", feature = "parse-dom"))]
+fn rustc_serialize_parse_dom(mut bytes: &[u8]) -> Result<rustc_serialize::json::Json, rustc_serialize::json::BuilderError> {
+    rustc_serialize::json::Json::from_reader(&mut bytes)
+}
+
+#[cfg(all(feature = "lib-rustc-serialize", feature = "parse-struct"))]
+fn rustc_serialize_parse_struct<T>(bytes: &[u8]) -> rustc_serialize::json::DecodeResult<T>
+    where T: rustc_serialize::Decodable
+{
+    use std::str;
+    let s = str::from_utf8(bytes).unwrap();
+    rustc_serialize::json::decode(s)
+}
+
+#[cfg(all(feature = "lib-rustc-serialize", any(feature = "stringify-dom", feature = "stringify-struct")))]
+fn rustc_serialize_stringify<W, T: ?Sized>(writer: W, value: &T) -> rustc_serialize::json::EncodeResult<()>
+    where W: Write,
+          T: rustc_serialize::Encodable
+{
+    let mut writer = adapter::IoWriteAsFmtWrite::new(writer);
+    let mut encoder = rustc_serialize::json::Encoder::new(&mut writer);
+    value.encode(&mut encoder)
 }
 
 fn millis(dur: time::Duration) -> i64 {
@@ -191,160 +245,4 @@ fn millis(dur: time::Duration) -> i64 {
 
 fn tenths(dur: time::Duration) -> u8 {
     (dur.num_microseconds().unwrap() / 100 % 10) as u8
-}
-
-#[cfg(feature = "lib-serde")]
-mod serde_json {
-    use super::*;
-    extern crate serde_json;
-
-    pub struct Library;
-
-    impl super::Library for Library {
-        type DOM = serde_json::Value;
-
-        fn name() -> &'static str { "serde_json" }
-
-        fn parse_dom<R: io::Read>(mut read: R) -> Result<serde_json::Value, ()> {
-            let mut string = String::new();
-            read.read_to_string(&mut string).unwrap();
-            serde_json::from_str(&string).or(Err(()))
-        }
-
-        fn stringify_dom<W: io::Write>(write: &mut W, dom: &serde_json::Value) {
-            serde_json::to_writer(write, &dom).unwrap();
-        }
-
-        fn can_codegen() -> bool { true }
-
-        fn parse_struct<R>(mut read: R, which: TestFile) -> Contents
-            where R: io::Read,
-        {
-            let mut string = String::new();
-            read.read_to_string(&mut string).unwrap();
-            match which {
-                #[cfg(feature = "file-canada")]
-                TestFile::Canada => {
-                    Contents::Canada(serde_json::from_str(&string).unwrap())
-                }
-                #[cfg(feature = "file-citm-catalog")]
-                TestFile::CitmCatalog => {
-                    Contents::CitmCatalog(serde_json::from_str(&string).unwrap())
-                }
-                #[cfg(feature = "file-twitter")]
-                TestFile::Twitter => {
-                    Contents::Twitter(serde_json::from_str(&string).unwrap())
-                }
-            }
-        }
-
-        #[cfg(any(feature = "file-canada",
-                  feature = "file-citm-catalog",
-                  feature = "file-twitter"))]
-        fn stringify_struct<W>(write: &mut W, obj: &Contents)
-            where W: io::Write,
-        {
-            match *obj {
-                #[cfg(feature = "file-canada")]
-                Contents::Canada(ref v) => serde_json::to_writer(write, v),
-                #[cfg(feature = "file-citm-catalog")]
-                Contents::CitmCatalog(ref v) => serde_json::to_writer(write, v),
-                #[cfg(feature = "file-twitter")]
-                Contents::Twitter(ref v) => serde_json::to_writer(write, v),
-            }.unwrap();
-        }
-    }
-}
-
-#[cfg(feature = "lib-json-rust")]
-mod json_rust {
-    use super::*;
-    extern crate json;
-
-    use self::json::JsonValue;
-
-    pub struct Library;
-
-    impl super::Library for Library {
-        type DOM = JsonValue;
-
-        fn name() -> &'static str { "json-rust" }
-
-        fn parse_dom<R: io::Read>(mut read: R) -> Result<JsonValue, ()> {
-            let mut string = String::new();
-            read.read_to_string(&mut string).unwrap();
-            json::parse(&string).or(Err(()))
-        }
-
-        fn stringify_dom<W: io::Write>(write: &mut W, dom: &JsonValue) {
-            dom.write(write).unwrap();
-        }
-    }
-}
-
-#[cfg(feature = "lib-rustc-serialize")]
-mod rustc {
-    use super::*;
-
-    use rustc_serialize::json;
-
-    pub struct Library;
-
-    impl super::Library for Library {
-        type DOM = json::Json;
-
-        fn name() -> &'static str { "rustc_serialize" }
-
-        fn parse_dom<R: io::Read>(mut read: R) -> Result<json::Json, ()> {
-            json::Json::from_reader(&mut read).or(Err(()))
-        }
-
-        fn stringify_dom<W: io::Write>(write: &mut W, dom: &json::Json) {
-            use rustc_serialize::Encodable;
-            let mut write = adapter::IoWriteAsFmtWrite::new(write);
-            dom.encode(&mut json::Encoder::new(&mut write)).unwrap();
-        }
-
-        fn can_codegen() -> bool { true }
-
-        fn parse_struct<R>(mut read: R, which: TestFile) -> Contents
-            where R: io::Read,
-        {
-            let mut string = String::new();
-            read.read_to_string(&mut string).unwrap();
-            match which {
-                #[cfg(feature = "file-canada")]
-                TestFile::Canada => {
-                    Contents::Canada(json::decode(&string).unwrap())
-                }
-                #[cfg(feature = "file-citm-catalog")]
-                TestFile::CitmCatalog => {
-                    Contents::CitmCatalog(json::decode(&string).unwrap())
-                }
-                #[cfg(feature = "file-twitter")]
-                TestFile::Twitter => {
-                    Contents::Twitter(json::decode(&string).unwrap())
-                }
-            }
-        }
-
-        #[cfg(any(feature = "file-canada",
-                  feature = "file-citm-catalog",
-                  feature = "file-twitter"))]
-        fn stringify_struct<W>(write: &mut W, obj: &Contents)
-            where W: io::Write,
-        {
-            use rustc_serialize::Encodable;
-            let mut write = adapter::IoWriteAsFmtWrite::new(write);
-            let mut encoder = json::Encoder::new(&mut write);
-            match *obj {
-                #[cfg(feature = "file-canada")]
-                Contents::Canada(ref v) => v.encode(&mut encoder),
-                #[cfg(feature = "file-citm-catalog")]
-                Contents::CitmCatalog(ref v) => v.encode(&mut encoder),
-                #[cfg(feature = "file-twitter")]
-                Contents::Twitter(ref v) => v.encode(&mut encoder),
-            }.unwrap();
-        }
-    }
 }
