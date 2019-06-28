ca52854ce7cb4baca26f37695312096482596d82
David Tolnay <dtolnay@gmail.com>
2019-06-28 09:55:35 -0700

    Remove notes in log::Log

--- src/borrow/log.rs
+++ src/borrow/log.rs
@@ -19,22 +19,22 @@ enum_str!(LogLevel {
 #[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
 pub struct Log<'a> {
     jk_host: &'a str,
-    class_name: String, //&'a str,
+    class_name: String,
     logger_name: &'a str,
-    cgi_tte_ms: &'a str, //"3173.81" kin of is a float
+    cgi_tte_ms: &'a str,
     start_timestamp: u64,
     user_agent_device: &'a str,
-    slush: String, //&'a str,
+    slush: String,
     and_an_ip4: &'a str,
     #[cfg_attr(feature = "lib-serde", serde(rename = "@version"))]
-    version: &'a str, // Could be u64? "1"
+    version: &'a str,
     error_url_path: &'a str,
     logstash: &'a str,
     #[cfg_attr(feature = "lib-serde", serde(rename = "uuids->"))]
     uuid: &'a str,
     anotherfilename: &'a str,
     environment: Environment,
-    floatasstr: &'a str, // Could be float "123.56",
+    floatasstr: &'a str,
     #[cfg_attr(feature = "lib-serde", serde(rename = "there_string:"))]
     there_string: &'a str,
     arry: Vec<&'a str>,
@@ -58,9 +58,9 @@ pub struct Log<'a> {
     controller: &'a str,
     key_keykeykey: &'a str,
     #[cfg_attr(feature = "lib-serde", serde(rename = "a proper_timestamp_ja"))]
-    proper_timestamp: &'a str, //Coudl be date? "2018-07-23T12:19:16-04:00",
+    proper_timestamp: &'a str,
     and_yet_another: &'a str,
     #[cfg_attr(feature = "lib-serde", serde(rename = "@timestamp"))]
-    timestamp: &'a str, //Could be date3 "2018-07-23T16:19:16.821Z",
+    timestamp: &'a str,
     level: u8,
 }
